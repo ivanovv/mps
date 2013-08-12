3fa38c9f0feee3cc334b39913f9abcb644ad9984
Victor Ivanov <vic.ivanoff@gmail.com>
2013-08-12 16:05:40 +0400

    Правильный класс для хайлайта

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,14 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.search params[:q]
     #@articles.context.panes << ThinkingSphinx::Panes::ExcerptsPane
-    @excerpter = ThinkingSphinx::Excerpter.new 'article_core', params[:q]
+    @excerpter = ThinkingSphinx::Excerpter.new(
+        'article_core',
+        params[:q],
+        {
+            :before_match => '<span class="app_search_word">',
+            :after_match  => '</span>'
+        }
+    )
 
     @articles = @articles.to_a
     @articles.each {|a| a.url = BASE_URL + URLS[(a.article_type || 'text')] + '/' + (a.url || '') }
